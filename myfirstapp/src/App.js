e7e9aa7d0cb9c4f5c3849fbef1fc2bedc7fd5ece
asmitakatwal <asmitakatwal246@gmail.com>
2023-09-13 11:55:43 +0545

    changes

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react';
 
-function App() {
+const App=()=> {
+  const [name,setName]=useState(['prafulla','sauhardha'])
+  const [inputFieldName,setInputFieldName]=useState('')
+  const addUsers=()=>{
+    const tempName=[...name]
+    tempName.push(inputFieldName)
+    setName(tempName)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<input 
+onInput={(event)=>setInputFieldName(event.target.value)}
+ placeholder="name"/>
+<button onClick={()=>addUsers()}>Add</button>
+{name.map((item,id)=>{
+return <li>{item}</li>
+})}
     </div>
   );
 }
